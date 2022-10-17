80a8a5cc7f70c1b97aa89af3a162182cbd5c1efd
amr21 <amryasser52001@gmail.com>
2022-10-18 01:18:17 +0200

    Final

--- Backupd/backupd.sh
+++ Backupd/backupd.sh
@@ -6,6 +6,15 @@ backupdir=$2
 interval=$3
 max=$4
 
+if [[ $dir = "" || $backupdir = "" || $interval = "" || $max = ""  ]]; 
+then 
+  clear
+  echo -e "\nSome arguments are empty, re-enter them\n"
+  make
+fi
+
+clear
+
 copyFile() {
   # Move to backup directory
   cd ../$backupdir
@@ -33,6 +42,20 @@ copyFile() {
 
   echo "Backup complete at $currentDate under $backupdir"
 }
+
+
+# Verify Numbers
+re='^[0-9]+$'
+
+if ! [[ $interval =~ $re ]] ; then
+   echo -e "\nError: Interval should be an integer\n";
+   make
+fi
+
+if ! [[ $max =~ $re ]] ; then
+   echo -e "\nError: Maximum should be an integer\n";
+   make
+fi
 
 # Get initial directory info
 ls -lR ../$dir >directory-info.last
